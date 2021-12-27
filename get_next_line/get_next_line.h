b5e7fe3fb03374c3e9b62d32cff15a2c7f28363e
EderTolentino <68605527+EderTolentino@users.noreply.github.com>
2021-12-27 17:43:02 +0000

    uploading the project almost read

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eluiz-to <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/27 16:06:49 by eluiz-to          #+#    #+#             */
+/*   Updated: 2021/12/27 16:06:53 by eluiz-to         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+//# include <stdio.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
